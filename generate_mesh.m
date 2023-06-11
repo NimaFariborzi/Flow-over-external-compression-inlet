3204bbb22a403958e2c61617cf9799281a26d550
michael7452 <michaelpuso@gmail.com>
2023-06-11 10:07:55 -0700

    Implement grid scale control in generate_mesh

--- generate_mesh.m
+++ generate_mesh.m
@@ -37,9 +37,12 @@ y2_dist = 2;             % Height of blocks 4,5
 % physical grid fairly uniform, but might get weird if changed much further from
 % a value of ~2 for each ratio
 
+% Computational grid scale
+gs = 1e-5;
+
 % Grid spacing
-d_xi = 0.1;  % Grid spacing in xi
-d_et = 0.05; % Grid spacing in eta
+d_xi = 0.1  * gs; % Grid spacing in xi
+d_et = 0.05 * gs; % Grid spacing in eta
 
 % Grid stretching
 alpha = 0.9;
@@ -49,15 +52,15 @@ alpha = 0.9;
 % ======================== %
 
 % Block one: computational grid
-xi1 = 0:d_xi:1;
-et1 = 0:d_et:1;
+xi1 = 0:d_xi:1*gs;
+et1 = 0:d_et:1*gs;
 [XI1, ET1] = ndgrid(xi1,et1);
 
 % Block one: physical grid
 X1 = XI1*x1_dist;
 Y1 = zeros(size(ET1));
 
-y_max = y1_dist;
+y_max = y1_dist*gs;
 for i = 1:length(xi1)
     x = X1(i,1);
     y_min = x*tand(delta1);
@@ -65,77 +68,77 @@ for i = 1:length(xi1)
 end
 
 % Block two: computational grid
-xi2 = 1:d_xi:3;
-et2 = 0:d_et:1; % Brutus?
+xi2 = 1*gs:d_xi:3*gs;
+et2 = 0:d_et:1*gs; % Brutus?
 [XI2, ET2] = ndgrid(xi2,et2);
 
 % Block two: physical grid
-X2 = rescale(XI2,x1_dist,x1_dist + x2_dist);
+X2 = rescale(XI2,x1_dist*gs,(x1_dist + x2_dist)*gs);
 Y2 = zeros(size(ET2));
 
-y_max = y1_dist;
+y_max = y1_dist*gs;
 for i = 1:length(xi2)
     x = X2(i,1);
-    y_min = x1_dist*tand(delta1) + (x - x1_dist)*tand(delta2);
+    y_min = x1_dist*gs*tand(delta1) + (x - x1_dist*gs)*tand(delta2);
     Y2(i,:) = TwoWayBiasY(Y2(i,:),y_min,y_max,alpha);
 end
 
 % Block three: computational grid
-xi3 = 3:d_xi:5;
-et3 = 0:d_et:1;
+xi3 = 3*gs:d_xi:5*gs;
+et3 = 0:d_et:1*gs;
 [XI3, ET3] = ndgrid(xi3,et3);
 
 % Block three: physical grid
-X3 = rescale(XI3,x1_dist + x2_dist,x1_dist + x2_dist + x3_dist);
+X3 = rescale(XI3,(x1_dist + x2_dist)*gs,(x1_dist + x2_dist + x3_dist)*gs);
 Y3 = zeros(size(ET3));
 
 y_min = Y2(end,1);
-y_max = y1_dist;
+y_max = y1_dist*gs;
 for i = 1:length(xi3)
     Y3(i,:) = TwoWayBiasY(Y3(i,:),y_min,y_max,alpha);
 end
 
 % Block four: computational grid
-xi4 = 0:d_xi:1;
-et4 = 1:d_et:2.2;
+xi4 = 0:d_xi:1*gs;
+et4 = 1*gs:d_et:2.2*gs;
 [XI4, ET4] = ndgrid(xi4,et4);
 
 % Block four: physical grid
 X4 = XI4*x1_dist;
 Y4 = zeros(size(ET4));
 
-y_min = y1_dist;
-y_max = y1_dist + y2_dist;
+y_min = y1_dist*gs;
+y_max = (y1_dist + y2_dist)*gs;
 for i = 1:length(xi4)
     Y4(i,:) = OneWayBiasY(Y4(i,:),y_min,y_max,alpha);
 end
 
 % Block five: computational grid
-xi5 = 1:d_xi:3;
-et5 = 1:d_et:2.2;
+xi5 = 1*gs:d_xi:3*gs;
+et5 = 1*gs:d_et:2.2*gs;
 [XI5, ET5] = ndgrid(xi5,et5);
 
 % Block five: physical grid
-X5 = rescale(XI5,x1_dist,x1_dist + x2_dist);
+X5 = rescale(XI5,x1_dist*gs,(x1_dist + x2_dist)*gs);
 Y5 = zeros(size(ET5));
 
-y_min = y1_dist;
-y_max = y1_dist + y2_dist;
+y_min = y1_dist*gs;
+y_max = (y1_dist + y2_dist)*gs;
 for i = 1:length(xi5)
     Y5(i,:) = OneWayBiasY(Y5(i,:),y_min,y_max,alpha);
 end
 
 % Block six: computational grid
-xi6 = 3:d_xi:5;
-et6 = 1:d_et:2.2;
+xi6 = 3*gs:d_xi:5*gs;
+et6 = 1*gs:d_et:2.2*gs;
 [XI6, ET6] = ndgrid(xi6,et6);
 
 % Block six: physical grid
-X6 = rescale(XI6,x1_dist + x2_dist,x1_dist + x2_dist + x3_dist);
+X6 = rescale(XI6,(x1_dist + x2_dist)*gs,(x1_dist + x2_dist + x3_dist)*gs);
 Y6 = zeros(size(ET6));
 
-y_min = y1_dist;
-y_max = y1_dist + y2_dist;
+y_min = y1_dist*gs;
+y_max = (y1_dist + y2_dist)*gs;
 for i = 1:length(xi6)
     Y6(i,:) = OneWayBiasY(Y6(i,:),y_min,y_max,alpha);
 end
