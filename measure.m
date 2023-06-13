c64f41404ed4e4cda746908b96adf91dd58529f0
michael7452 <michaelpuso@gmail.com>
2023-06-13 14:32:59 -0700

    Added M=3 case to measure.m

--- measure.m
+++ measure.m
@@ -7,12 +7,19 @@ clc; clear; close all
 % delta2 = 21.539;
 % beta2 = 51.898;
 
-% Modified ramp for M = 4
-delta1 = 16.1345;
-beta1 = 28.2389;
+% Modified ramp for M = 3
+delta1 = 14.9766;
+beta1 = 32.2162;
 
-delta2 = 22.1015;
-beta2 = 41.6599;
+delta2 = 18.8126;
+beta2 = 45.1465;
+
+% Modified ramp for M = 4
+% delta1 = 16.1345;
+% beta1 = 28.2389;
+% 
+% delta2 = 22.1015;
+% beta2 = 41.6599;
 
 alpha1 = beta1 - delta1;
 alpha2 = beta2 - delta2;
@@ -28,4 +35,7 @@ a3 = sind(alpha1)*a1/sind(theta1);
 b1 = a3*cosd(alpha2);
 x2 = b1*cosd(delta2)
 
-y = a1*sind(delta1) + a3*sind(beta2)
+y_int = a1*sind(delta1) + a3*sind(beta2)
+
+% x value of shock intersection
+x_int = x1 + a3*cosd(beta2)
